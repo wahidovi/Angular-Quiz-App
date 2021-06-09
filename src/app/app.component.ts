70a771452eebbc68eb73e517bb622a3a9730d60e
Wahid Ovi <md.wahidovi@gmail.com>
2021-06-09 00:52:37 -0700

    final rush

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+// This is the root component of the app
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  template: '<router-outlet></router-outlet>'
+})
+export class AppComponent {}
